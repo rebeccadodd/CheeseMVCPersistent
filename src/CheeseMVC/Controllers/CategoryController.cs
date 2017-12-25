456396c4cfe048d924c483fd3c34715574b42d7e
doddy <rebecca.dodd@gmail.com>
2017-12-25 08:35:41 -0600

    Completed Part 1: Persisting a Single Class

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<CheeseCategory> cheeseCategories = context.Categories.ToList();
+
+            return View(cheeseCategories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(addCategoryViewModel);
+            }
+            //Add new category to my existing categories
+            CheeseCategory newCategory = new CheeseCategory
+            {
+                Name = addCategoryViewModel.Name
+            };
+
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+
+            return Redirect("/Category");
+        }
+    }
+}
