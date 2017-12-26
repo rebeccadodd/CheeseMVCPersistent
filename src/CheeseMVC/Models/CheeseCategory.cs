d4e786602f35bb30d81c86d55a521c018b656441
doddy <rebecca.dodd@gmail.com>
2017-12-25 18:37:13 -0600

    Completed Part 2: Setting Up a One-to-Many Relationship

--- src/CheeseMVC/Models/CheeseCategory.cs
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -1,8 +1,12 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class CheeseCategory
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+        IList<Cheese> Cheeses { get; set; }
     }
 }
